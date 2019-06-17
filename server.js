122ff3fb4db77283a33cb30a07dd9de413b9a63e
Ty Wood <twood06@gmail.com>
2019-06-17 14:26:24 -0400

    adding models

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ var cheerio = require('cheerio')
 
 
 var PORT = 3000
+
 var app = express
 
+var db = require('./models')
+
+
+//connecting to heroku with mongo
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
@@ -15,6 +20,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
+
+//routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!")
